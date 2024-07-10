575a874c7c17936dda063f633cf8ccb0d4c4a06c
Pardo <sarah.pardo03@gmail.com>
2024-07-10 16:57:05 +0200

    add about page

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+export function About(){
+  return <>
+    <Container className="about-container">
+      <Row>
+        <Col>1 of 1</Col>
+      </Row>
+    </Container>
+  </>
+}
