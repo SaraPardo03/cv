8c0d14c9fa0d6376faff7a7eebe2b9b2ec8fc68f
Pardo <sarah.pardo03@gmail.com>
2024-07-11 23:53:49 +0200

    add about text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,14 @@ export function About(){
   return <>
     <Container className="about-container">
       <Row>
-        <Col>1 of 1</Col>
+        <Col>
+        <h1>L'art du code</h1>
+        <p>Depuis 2008, je développe des applications utiles et ergonomiques. Grâce à ma formation et mon expérience professionnelle, je transforme des problèmes complexes en solutions simples et efficaces. J'accorde une attention particulière aux détails pour produire du code propre et performant.</p>
+        </Col>
+        <Col>
+        <h1>L'algorithme créatif</h1>
+        <p>En parallèle de mon travail, je suis passionnée par le dessin et le design graphique. J'aime explorer de nouvelles idées et créer des œuvres visuellement captivantes et intuitives.</p>
+        </Col>
       </Row>
     </Container>
   </>
