2f0dff5e564bce1d1f4a2ac6326ed190b47caa11
Pardo <sarah.pardo03@gmail.com>
2024-07-17 18:29:36 +0200

    Add text on page experience and educations

--- src/pages/Educations.jsx
+++ src/pages/Educations.jsx
@@ -1,48 +1,79 @@
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-export function Education(){
+export function Educations(){
   return <>
-  <section className='educations-container p-2 container font-figtree'>
+  <section className='experiences-container p-2 container font-figtree'>
     <Row>
       <Col className='d-block d-md-none col-12 text-left mb-2'>
-        <h2 className='fw-bold'>Expériences</h2>
+        <h2 className='fw-bold'>Formations</h2>
       </Col>
       <Col className='d-none d-md-block col-12 text-center mb-2'>
-        <h2 className='fw-bold'>Expériences</h2>
+        <h2 className='fw-bold'>Formations</h2>
       </Col>
     </Row>
     <section className='experiences-list p-0 ps-1 pe-1'>
       <Row>
         <Col className='d-none d-md-block'>
-          <h6 className='fw-light text-uppercase text-end p-0 pt-2'>Current - June 2023</h6>
+          <h6 className='fw-light text-uppercase text-end p-0 pt-2'>mai- juin 2024</h6>
         </Col>
         <Col className='time-line p-0 pt-2'>
           <h6 className='d-block d-md-none text-white p-0'>'</h6>
           <h3 className='dot'></h3>
         </Col>
         <Col className=''>
-          <h6 className='d-block d-md-none fw-light text-uppercase  p-0 pt-2'>Current - June 2023</h6>
-          <h4 className='title fw-bold p-0 mb-0'>Senior Developer @ Startselect (fulltime)</h4>
-          <span className='d-block mb-4'>Eindhoven | <a href='www.goole.com'>www.google.com</a></span>
-          <p className='mb-5'>At Startselect, I was a Fulltime Senior Developer working on a large, global webshop for digital games, gift cards, and other digital entertainment. The application was based on Laravel, and as a team, we followed the Scrum methodology for our work. As a senior, I provided assistance to various departments within the company to help them achieve their goals.</p>
+          <h6 className='d-block d-md-none fw-light text-uppercase  p-0 pt-2'>mai- juin 2024</h6>
+          <h4 className='title fw-bold p-0 mb-0'>Formation Python</h4>
+          <span className='d-block mb-4'>Nomades advances technologies | Mention : Très bien </span>
+          <p className='mb-5'>7 semaines de formation intensive en Python, couvrant les fondamentaux du langage, le développement web avec Flask, les Rest APIs, Numpy, Pandas et la conception d'applications. Projet de certification: développement de l'API REST pour "Ariane", un moteur de jeu textuel interactif.</p>
         </Col>
       </Row>
     </section>
     <section className='experiences-list p-0 ps-1 pe-1'>
       <Row>
         <Col className='d-none d-md-block'>
-          <h6 className='fw-light text-uppercase text-end p-0 pt-2'>Current - June 2023</h6>
+          <h6 className='fw-light text-uppercase text-end p-0 pt-2'>février- mars 2024</h6>
         </Col>
         <Col className='time-line p-0 pt-2'>
           <h6 className='d-block d-md-none text-white p-0'>'</h6>
           <h3 className='dot'></h3>
         </Col>
         <Col className=''>
-          <h6 className='d-block d-md-none fw-light text-uppercase  p-0 pt-2'>Current - June 2023</h6>
-          <h4 className='fw-bold p-0 mb-0'>Senior Developer @ Startselect (fulltime)</h4>
-          <span className='d-block mb-4'>Eindhoven | <a href='www.goole.com'>www.google.com</a></span>
-          <p className='mb-5'>At Startselect, I was a Fulltime Senior Developer working on a large, global webshop for digital games, gift cards, and other digital entertainment. The application was based on Laravel, and as a team, we followed the Scrum methodology for our work. As a senior, I provided assistance to various departments within the company to help them achieve their goals.</p>
+          <h6 className='d-block d-md-none fw-light text-uppercase  p-0 pt-2'>février- mars 2024</h6>
+          <h4 className='fw-bold p-0 mb-0'>Formation REACT</h4>
+          <span className='d-block mb-4'>Nomades advances technologies | Mention : Très bien</span>
+          <p className='mb-5'>Formation intensive de 6 semaines en JavaScript, React et Firebase. Projet de certification : développement de la partie front-end d’un moteur de jeu textuel destiné à aider les auteurs d'histoires interactives.</p></Col>
+      </Row>
+    </section>
+    <section className='experiences-list p-0 ps-1 pe-1'>
+      <Row>
+        <Col className='d-none d-md-block'>
+          <h6 className='fw-light text-uppercase text-end p-0 pt-2'>2013-2014</h6>
+        </Col>
+        <Col className='time-line p-0 pt-2'>
+          <h6 className='d-block d-md-none text-white p-0'>'</h6>
+          <h3 className='dot'></h3>
+        </Col>
+        <Col className=''>
+          <h6 className='d-block d-md-none fw-light text-uppercase  p-0 pt-2'>2013-2014</h6>
+          <h4 className='fw-bold p-0 mb-0'>Technicienne en informatique </h4>
+          <span className='d-block mb-4'>CFPT 1er année | Genève</span>
+        </Col>
+      </Row>
+    </section>
+    <section className='experiences-list p-0 ps-1 pe-1'>
+      <Row>
+        <Col className='d-none d-md-block'>
+          <h6 className='fw-light text-uppercase text-end p-0 pt-2'>2008-2013</h6>
+        </Col>
+        <Col className='time-line p-0 pt-2'>
+          <h6 className='d-block d-md-none text-white p-0'>'</h6>
+          <h3 className='dot'></h3>
+        </Col>
+        <Col className=''>
+          <h6 className='d-block d-md-none fw-light text-uppercase  p-0 pt-2'>2008-2013</h6>
+          <h4 className='fw-bold p-0 mb-0'>CFC d’informaticienne</h4>
+          <span className='d-block mb-4'>CFPT | Genève</span>
         </Col>
       </Row>
     </section>
--- src/pages/Experiences.jsx
+++ src/pages/Experiences.jsx
@@ -15,35 +15,33 @@ export function Experiences(){
     <section className='experiences-list p-0 ps-1 pe-1'>
       <Row>
         <Col className='d-none d-md-block'>
-          <h6 className='fw-light text-uppercase text-end p-0 pt-2'>Current - June 2023</h6>
+          <h6 className='fw-light text-uppercase text-end p-0 pt-2'>2016 - 2022</h6>
         </Col>
         <Col className='time-line p-0 pt-2'>
           <h6 className='d-block d-md-none text-white p-0'>'</h6>
           <h3 className='dot'></h3>
         </Col>
         <Col className=''>
-          <h6 className='d-block d-md-none fw-light text-uppercase  p-0 pt-2'>Current - June 2023</h6>
-          <h4 className='title fw-bold p-0 mb-0'>Senior Developer @ Startselect (fulltime)</h4>
-          <span className='d-block mb-4'>Eindhoven | <a href='www.goole.com'>www.google.com</a></span>
-          <p className='mb-5'>At Startselect, I was a Fulltime Senior Developer working on a large, global webshop for digital games, gift cards, and other digital entertainment. The application was based on Laravel, and as a team, we followed the Scrum methodology for our work. As a senior, I provided assistance to various departments within the company to help them achieve their goals.</p>
-        </Col>
+          <h6 className='d-block d-md-none fw-light text-uppercase  p-0 pt-2'>2016 - 2022</h6>
+          <h4 className='title fw-bold p-0 mb-0'>Développeuse full-stack </h4>
+          <span className='d-block mb-4'>Topnet Sa | Genève</span>
+          <p className='mb-5'>J’ai contribué au développement d'applications métiers en utilisant EXTJS, JavaScript, HTML5, CSS3 et le modèle MVC pour le front-end, ainsi que PHP, SQL, Bash et la POO pour le back-end. J'ai analysé et intégré des processus RH, commerciaux et d'exploitation, et réalisé des tests pour garantir la qualité des développements. J'ai estimé et planifié les projets, rédigé des documentations techniques et des guides utilisateurs, formé et supporté les utilisateurs, et conçu des maquettes d'écrans. J'ai également géré les versions avec GIT, assurant une collaboration efficace et la continuité des développements.</p></Col>
       </Row>
     </section>
     <section className='experiences-list p-0 ps-1 pe-1'>
       <Row>
         <Col className='d-none d-md-block'>
-          <h6 className='fw-light text-uppercase text-end p-0 pt-2'>Current - June 2023</h6>
+          <h6 className='fw-light text-uppercase text-end p-0 pt-2'>2008 - 2013</h6>
         </Col>
         <Col className='time-line p-0 pt-2'>
           <h6 className='d-block d-md-none text-white p-0'>'</h6>
           <h3 className='dot'></h3>
         </Col>
         <Col className=''>
-          <h6 className='d-block d-md-none fw-light text-uppercase  p-0 pt-2'>Current - June 2023</h6>
-          <h4 className='fw-bold p-0 mb-0'>Senior Developer @ Startselect (fulltime)</h4>
-          <span className='d-block mb-4'>Eindhoven | <a href='www.goole.com'>www.google.com</a></span>
-          <p className='mb-5'>At Startselect, I was a Fulltime Senior Developer working on a large, global webshop for digital games, gift cards, and other digital entertainment. The application was based on Laravel, and as a team, we followed the Scrum methodology for our work. As a senior, I provided assistance to various departments within the company to help them achieve their goals.</p>
-        </Col>
+          <h6 className='d-block d-md-none fw-light text-uppercase  p-0 pt-2'>2008 - 2013</h6>
+          <h4 className='fw-bold p-0 mb-0'>Apprentie informaticienne</h4>
+          <span className='d-block mb-4'>Hyl-Informatique | Genève</span>
+          <p className='mb-5'>J'ai développé des sites web dynamiques et responsives avec HTML5, CSS3, PHP et SQL, tout en gérant des bases de données et en assurant des sauvegardes et restaurations. J'ai également fourni un support technique et une aide à la clientèle de qualité, tout en vendant, installant et configurant du matériel informatique.</p></Col>
       </Row>
     </section>
   </section>
