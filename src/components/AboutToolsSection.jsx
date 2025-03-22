58db4bddea7395a461970ffd852d2a38e95060a2
Pardo <sarah.pardo03@gmail.com>
2025-03-22 02:25:16 +0100

    Add tools section on about page

--- src/components/AboutToolsSection.jsx
+++ src/components/AboutToolsSection.jsx
@@ -1,76 +1,31 @@
-
 import SkillBar from "./SkillsBar";
-import Tag from "./Tag";
 
-function AboutToolsSection(){
+function AboutToolsSection({phpTools, pythonTools, bashTools, bddTools, htmlCss, javascript, uiUx}){
   return <>
-    <section id="about-tools-section-mobile" className="hidden lg:block">
-      <div className="back-end-tools px-[25px] pt-[25px] color-text-dark">
-        <h1 className="title">Outils <span>back-end</span></h1>
+    <section id="about-tools-section" className="hidden lg:flex flex flex-row ">
+      <div className="back-end-tools flex-1 px-[25px] color-text-dark pt-[100px]">
+        <h2 className="subtitle pale">Outils</h2>
+        <h1 className="title deep mb-[10px]">Développeuse <span>back-end</span></h1>
         <p>Développement d’APIs, gestion des bases de données et sécurité.</p>
-        <div className="about-tool-container py-[25px]">
-          <SkillBar skill="PHP" pourcentage="80" label="+ 10 ans"/>
-          <ul className="flex flex-wrap gap-[10px] mt-[15px]">
-            <Tag label="PHP 7/8"/>
-            <Tag label="POO"/>
-            <Tag label="API REST"/>
-            <Tag label="JSON/XML"/>
-            <Tag label="Sessions"/>
-            <Tag label="Sécurité"/>
-            <Tag label="Postman"/>
-          </ul>
-        </div>
-        <div className="about-tool-container py-[25px]">
-          <SkillBar skill="Python" pourcentage="20" label="1 ans"/>
-          <ul className="flex flex-wrap gap-[10px] mt-[15px]">
-            <Tag label="Python 3"/>
-            <Tag label="API REST"/>
-            <Tag label="Flask"/>
-            <Tag label="Django"/>
-            <Tag label="Pandas"/>
-            <Tag label="NumPy"/>
-            <Tag label="JWT"/>
-          </ul>
-        </div>
-        <div className="about-tool-container py-[25px]">
-          <SkillBar skill="BASH" pourcentage="50" label="+ 10 ans"/>
-          <ul className="flex flex-wrap gap-[10px] mt-[15px]">
-            <Tag label="Scripting"/>
-            <Tag label="Automatisation"/>
-            <Tag label="Permissions"/>
-            <Tag label="Cron"/>
-          </ul>
-        </div>
-        <div className="about-tool-container py-[25px]">
-          <SkillBar skill="Base de données" pourcentage="80" label="+ 10 ans"/>
-          <ul className="flex flex-wrap gap-[10px] mt-[15px]">
-            <Tag label="SQL"/>
-            <Tag label="NoSQL"/>
-            <Tag label="MySQL"/>
-            <Tag label="MongoDB"/>
-            <Tag label="Firebase"/>
-            <Tag label="Optimisation"/>
-          </ul>
-        </div>
-        <div className="about-tool-container py-[25px]">
-          <SkillBar skill="Méthodes" pourcentage="50" label="+ 10 ans"/>
-          <ul className="flex flex-wrap gap-[10px] mt-[15px]">
-            <Tag label="Git"/>
-            <Tag label="GitHub"/>
-            <Tag label="Bitbucket"/>
-            <Tag label="Trello"/>
-            <Tag label="Slack"/>
-            <Tag label="Gantt"/>
-            <Tag label="UML"/>
-            <Tag label="Agile"/>
-            <Tag label="Kanban"/>
-            <Tag label="SEO"/>
-          </ul>
+        <div className="about-tool-container flex flex-wrap gap-[20px] py-[25px]">
+          <SkillBar skill="PHP" pourcentage="75" label="+ 10 ans" tools={phpTools} collapseNotAllowed={true}/>
+          <SkillBar skill="PYTHON" pourcentage="30" label="1 ans" tools={pythonTools} collapseNotAllowed={true}/>
+          <SkillBar skill="BASH" pourcentage="50" label="5 ans" tools={bashTools} collapseNotAllowed={true}/>
+          <SkillBar skill="BDD" pourcentage="75" label="+ 10 ans" tools={bddTools} collapseNotAllowed={true}/>
         </div>
       </div>
-      <div className="front-end-tools px-[25px] pt-[25px] color-text-light">
-        <h1 className="title">Outils <span>front-end</span></h1>
+      <div className="clip flex-none w-[50px]">
+        <div className="bug"></div>
+      </div>
+      <div className="front-end-tools flex-1 px-[25px] pt-[100px] color-text-light">
+        <h2 className="subtitle pale">Outils</h2>
+        <h1 className="title deep  mb-[10px]">Développeuse <span>front-end</span></h1>
         <p>Création d’interfaces optimisées et centrées sur l’expérience utilisateur.</p>
+        <div className="about-tool-container flex flex-wrap gap-[20px] py-[25px]">
+          <SkillBar skill="HTML/CSS" pourcentage="80" label="+ 10 ans" tools={htmlCss} toolsColor="light" collapseNotAllowed={true}/>
+          <SkillBar skill="JAVASCRIPT" pourcentage="20" label="1 ans" tools={javascript} toolsColor="light" collapseNotAllowed={true}/>
+          <SkillBar skill="iUx" pourcentage="50" label="5 ans" tools={uiUx} toolsColor="light" collapseNotAllowed={true}/>
+        </div>
       </div>
     </section>
   </>
--- src/components/AboutToolsSectionMobile.jsx
+++ src/components/AboutToolsSectionMobile.jsx
@@ -1,76 +1,27 @@
-
 import SkillBar from "./SkillsBar";
-import Tag from "./Tag";
 
-function AboutToolsSectionMobile(){
+function AboutToolsSectionMobile({phpTools, pythonTools, bashTools, bddTools, htmlCss, javascript, uiUx}){
+  
   return <>
     <section id="about-tools-section-mobile" className="lg:hidden">
-      <div className="back-end-tools px-[25px] pt-[25px] color-text-dark">
+      <div className="back-end-tools px-[25px] pt-[120px] color-text-dark">
         <h1 className="title">Outils <span>back-end</span></h1>
         <p>Développement d’APIs, gestion des bases de données et sécurité.</p>
-        <div className="about-tool-container py-[25px]">
-          <SkillBar skill="PHP" pourcentage="80" label="+ 10 ans"/>
-          <ul className="flex flex-wrap gap-[10px] mt-[15px]">
-            <Tag label="PHP 7/8"/>
-            <Tag label="POO"/>
-            <Tag label="API REST"/>
-            <Tag label="JSON/XML"/>
-            <Tag label="Sessions"/>
-            <Tag label="Sécurité"/>
-            <Tag label="Postman"/>
-          </ul>
-        </div>
-        <div className="about-tool-container py-[25px]">
-          <SkillBar skill="Python" pourcentage="20" label="1 ans"/>
-          <ul className="flex flex-wrap gap-[10px] mt-[15px]">
-            <Tag label="Python 3"/>
-            <Tag label="API REST"/>
-            <Tag label="Flask"/>
-            <Tag label="Django"/>
-            <Tag label="Pandas"/>
-            <Tag label="NumPy"/>
-            <Tag label="JWT"/>
-          </ul>
-        </div>
-        <div className="about-tool-container py-[25px]">
-          <SkillBar skill="BASH" pourcentage="50" label="+ 10 ans"/>
-          <ul className="flex flex-wrap gap-[10px] mt-[15px]">
-            <Tag label="Scripting"/>
-            <Tag label="Automatisation"/>
-            <Tag label="Permissions"/>
-            <Tag label="Cron"/>
-          </ul>
-        </div>
-        <div className="about-tool-container py-[25px]">
-          <SkillBar skill="Base de données" pourcentage="80" label="+ 10 ans"/>
-          <ul className="flex flex-wrap gap-[10px] mt-[15px]">
-            <Tag label="SQL"/>
-            <Tag label="NoSQL"/>
-            <Tag label="MySQL"/>
-            <Tag label="MongoDB"/>
-            <Tag label="Firebase"/>
-            <Tag label="Optimisation"/>
-          </ul>
-        </div>
-        <div className="about-tool-container py-[25px]">
-          <SkillBar skill="Méthodes" pourcentage="50" label="+ 10 ans"/>
-          <ul className="flex flex-wrap gap-[10px] mt-[15px]">
-            <Tag label="Git"/>
-            <Tag label="GitHub"/>
-            <Tag label="Bitbucket"/>
-            <Tag label="Trello"/>
-            <Tag label="Slack"/>
-            <Tag label="Gantt"/>
-            <Tag label="UML"/>
-            <Tag label="Agile"/>
-            <Tag label="Kanban"/>
-            <Tag label="SEO"/>
-          </ul>
+        <div className="about-tool-container flex flex-wrap gap-[20px] py-[25px]">
+          <SkillBar skill="PHP" pourcentage="75" label="+ 10 ans" tools={phpTools}/>
+          <SkillBar skill="PYTHON" pourcentage="30" label="1 ans" tools={pythonTools}/>
+          <SkillBar skill="BASH" pourcentage="50" label="5 ans" tools={bashTools}/>
+          <SkillBar skill="BDD" pourcentage="75" label="+ 10 ans" tools={bddTools}/>
         </div>
       </div>
       <div className="front-end-tools px-[25px] pt-[25px] color-text-light">
         <h1 className="title">Outils <span>front-end</span></h1>
         <p>Création d’interfaces optimisées et centrées sur l’expérience utilisateur.</p>
+        <div className="about-tool-container flex flex-wrap gap-[20px] py-[25px]">
+          <SkillBar skill="HTML/CSS" pourcentage="80" label="+ 10 ans" tools={htmlCss} toolsColor="light"/>
+          <SkillBar skill="JAVASCRIPT" pourcentage="20" label="1 ans" tools={javascript} toolsColor="light"/>
+          <SkillBar skill="iUx" pourcentage="50" label="5 ans" tools={uiUx} toolsColor="light"/>
+        </div>
       </div>
     </section>
   </>
